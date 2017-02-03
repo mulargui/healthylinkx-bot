45d2e93a681ff19def3ecd8bcad12f4826394190
mulargui <mauricio@ulargui.com>
2017-02-03 15:33:41 -0800

    setting up the basics

---
+++ index.js
@@ -0,0 +1,32 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# RUN THE BOT:
+  Get a Bot token from Slack:
+    -> http://my.slack.com/services/new/bot
+  Run your bot from the command line:
+    token=<MY TOKEN> node index.js
+# EXTEND THE BOT:
+  Botkit has many features for building cool and useful bots!
+  Read all about it here:
+    -> http://howdy.ai/botkit
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+  debug: false
+  //include "log: false" to disable logging
+  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+	console.log("Starting the bot...");	
+	//token: process.env.token,
+	token: xoxb-136859518402-ehSDbBTESKJ4PQi5bhC4uRw1,
+}).startRTM()
+
+// give the bot something to listen for.
+controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+	console.log("Message received");	
+	bot.reply(message,'Hello yourself.');
+});
