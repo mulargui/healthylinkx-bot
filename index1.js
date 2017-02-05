4d9fe7c97dac35779a38d4130a3dfab7aedaddb6
mulargui <mauricio@ulargui.com>
2017-02-04 17:32:56 -0800

    Basic conversation implemented

---
+++ index1.js
@@ -0,0 +1,51 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# RUN THE BOT:
+  Get a Bot token from Slack:
+    -> http://my.slack.com/services/new/bot
+  Run your bot from the command line:
+    token=<MY TOKEN> node index.js
+# EXTEND THE BOT:
+  Botkit has many features for building cool and useful bots!
+  Read all about it here:
+    -> http://howdy.ai/botkit
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+  debug: false
+  //include "log: false" to disable logging
+  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+	token: process.env.token,
+}).startRTM()
+
+//handle all incoming messages
+// Note: Platforms such as Slack send many kinds of messages, not all of which contain a text field!
+//controller.on('message_received', function(bot, message) {
+	//bot.reply(message, 'I heard... something!');
+//});
+
+// reply to a mention
+controller.on('mention',function(bot,message) {
+  bot.reply(message,'Grabbing the mention');
+});
+
+// reply to a direct mention - @bot hello
+controller.on('direct_mention',function(bot,message) {
+  bot.reply(message,'I heard you mention me!');
+});
+
+// reply to a direct message
+controller.on('direct_message',function(bot,message) {
+  bot.reply(message,'You are talking directly to me');
+});
+
+// give the bot something to listen for.
+/*controller.hears(['hello', 'hi'], ['direct_message','direct_mention','mention'], function(bot,message) {
+	console.log("Message received");	
+	bot.reply(message,'Hello yourself.');
+});*/
--- index2.js
+++ index2.js
@@ -1,140 +1,88 @@
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-This is a sample Slack Button application that adds a bot to one or many slack teams.
-# RUN THE APP:
-  Create a Slack app. Make sure to configure the bot user!
-    -> https://api.slack.com/applications/new
-    -> Add the Redirect URI: http://localhost:3000/oauth
-  Run your bot from the command line:
-    clientId=<my client id> clientSecret=<my client secret> port=3000 node slackbutton_bot.js
-# USE THE APP
-  Add the app to your Slack by visiting the login page:
-    -> http://localhost:3000/login
-  After you've added the app, try talking to your bot!
-# EXTEND THE APP:
-  Botkit has many features for building cool and useful bots!
-  Read all about it here:
-    -> http://howdy.ai/botkit
+# RUN THE BOT:
+	Get a Bot token from Slack:
+		-> http://my.slack.com/services/new/bot
+	Run your bot from the command line:
+		token=<MY TOKEN> node index.js
+# EXTEND THE BOT:
+	Botkit has many features for building cool and useful bots!
+	Read all about it here:
+		-> http://howdy.ai/botkit
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-/* Uses the slack button feature to offer a real time bot to multiple teams */
-var Botkit = require('../lib/Botkit.js');
-
-if (!process.env.clientId || !process.env.clientSecret || !process.env.port) {
-  console.log('Error: Specify clientId clientSecret and port in environment');
-  process.exit(1);
-}
-
+var Botkit = require('botkit');
 
 var controller = Botkit.slackbot({
-  json_file_store: './db_slackbutton_bot/',
-  // rtm_receive_messages: false, // disable rtm_receive_messages if you enable events api
-}).configureSlackApp(
-  {
-    clientId: process.env.clientId,
-    clientSecret: process.env.clientSecret,
-    redirectUri: process.env.redirectUri, // optional parameter passed to slackbutton oauth flow
-    scopes: ['bot'],
-  }
-);
-
-controller.setupWebserver(process.env.port,function(err,webserver) {
-  controller.createWebhookEndpoints(controller.webserver);
-
-  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
-    if (err) {
-      res.status(500).send('ERROR: ' + err);
-    } else {
-      res.send('Success!');
-    }
-  });
+	debug: false
+	//include "log: false" to disable logging
+	//or a "logLevel" integer from 0 to 7 to adjust logging verbosity
 });
 
+// connect the bot to a stream of messages
+controller.spawn({
+	token: process.env.token,
+}).startRTM()
 
-// just a simple way to make sure we don't
-// connect to the RTM twice for the same team
-var _bots = {};
-function trackBot(bot) {
-  _bots[bot.config.token] = bot;
+// give the bot something to listen for.
+controller.hears(['hello', 'hi'], ['direct_message','direct_mention','mention'], function(bot,message) {
+	bot.startConversation(message, step0);
+});
+
+step0 = function(response, convo) {
+	convo.ask('step0?', [
+	{
+		pattern: 'done',
+		callback: function(response,convo) {
+			convo.say('OK you are done!');
+			convo.next();
+		}
+	},
+	{
+		pattern: 'yes',
+		callback: function(response,convo) {
+			convo.say('Great! I will continue...');
+			step1(response, convo);
+			convo.next();
+		}
+	},
+	{
+		pattern: 'no',
+		callback: function(response,convo) {
+			convo.say('Perhaps later.');
+			// do something else...
+			convo.next();
+		}
+	},
+	{
+		default: true,
+		callback: function(response,convo) {
+			// just repeat the question
+			convo.repeat();
+			convo.next();
+		}
+	}]);
 }
 
-controller.on('create_bot',function(bot,config) {
+step1 = function(response, convo) {
+	convo.ask('step1?', function(response, convo) {
+		convo.say('Awesome, you said: #' + response.text + '#');
+		step2(response, convo);
+		convo.next();
+	});
+}
 
-  if (_bots[bot.config.token]) {
-    // already online! do nothing.
-  } else {
-    bot.startRTM(function(err) {
+step2 = function(response, convo) {
+	convo.ask('step2?', function(response, convo) {
+		convo.say('Ok, you said: #' + response.text + '#');
+		step3(response, convo);
+		convo.next();
+	});
+}
 
-      if (!err) {
-        trackBot(bot);
-      }
+step3 = function(response, convo) {
+	convo.ask('step3?', function(response, convo) {
+		convo.say('Ok, you said: #' + response.text + '#Good bye.');
+		convo.next();
+	});
+}
 
-      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
-        if (err) {
-          console.log(err);
-        } else {
-          convo.say('I am a bot that has just joined your team');
-          convo.say('You must now /invite me to a channel so that I can be of use!');
-        }
-      });
-
-    });
-  }
-
-});
-
-
-// Handle events related to the websocket connection to Slack
-controller.on('rtm_open',function(bot) {
-  console.log('** The RTM api just connected!');
-});
-
-controller.on('rtm_close',function(bot) {
-  console.log('** The RTM api just closed');
-  // you may want to attempt to re-open
-});
-
-controller.hears('hello','direct_message',function(bot,message) {
-  bot.reply(message,'Hello!');
-});
-
-controller.hears('^stop','direct_message',function(bot,message) {
-  bot.reply(message,'Goodbye');
-  bot.rtm.close();
-});
-
-controller.on(['direct_message','mention','direct_mention'],function(bot,message) {
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'robot_face',
-  },function(err) {
-    if (err) { console.log(err) }
-    bot.reply(message,'I heard you loud and clear boss.');
-  });
-});
-
-controller.storage.teams.all(function(err,teams) {
-
-  if (err) {
-    throw new Error(err);
-  }
-
-  // connect all teams with bots up to slack!
-  for (var t  in teams) {
-    if (teams[t].bot) {
-      controller.spawn(teams[t]).startRTM(function(err, bot) {
-        if (err) {
-          console.log('Error connecting bot to Slack:',err);
-        } else {
-          trackBot(bot);
-        }
-      });
-    }
-  }
-
-});
